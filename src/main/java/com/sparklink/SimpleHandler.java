226492e42b34e571dfbe99ae9209d551c8a73111
sparklink-ts <sparklink.ts@gmail.com>
2026-02-08 22:59:28 +0530

    String functions : toLowerCase() and String Concat is used

--- src/main/java/com/sparklink/SimpleHandler.java
+++ src/main/java/com/sparklink/SimpleHandler.java
@@ -9,8 +9,16 @@ public class SimpleHandler implements RequestHandler<String,String> {
 
     public String handleRequest(String input, Context context){
         LambdaLogger logger = context.getLogger();
+        logger.log("Input string : " + input);
+        return processRequest(input,context);
+    }
+
+    public String processRequest(String input, Context context){
+        LambdaLogger logger = context.getLogger();
         logger.log("JDK Version: " + System.getProperty("java.version"));
-        return input.toUpperCase(); // Convert the input string to Upper Case
+        //return input.toUpperCase(); // Convert the input string to Upper Case
+        //return input.toLowerCase(); // Convert the input string to Lower Case
+        return "This is an example of AWS Lambda !!! " + input; // Concat the custom String with input string.
     }
 
 }
